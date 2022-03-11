dcc0fa7745e8709e7a0467d5f0b6738d0835991b
IBRAHIMKAMAL123 <99192251+IBRAHIMKAMAL123@users.noreply.github.com>
2022-03-11 07:35:08 +0530

    commit

---
+++ 2022-03-10/task.py
@@ -0,0 +1,45 @@
+Data loading ajax URL
+	Request URL: https://www.flipkart.com/audio-video/~cs-53mrbtcuf5/pr?sid=0pm&collection-tab-name=Audio+And+Video&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&p%5B%5D=facets.availability%255B%255D%3DExclude%2BOut%2Bof%2BStock&p%5B%5D=facets.rating%255B%255D%3D4%25E2%2598%2585%2B%2526%2Babove&fm=neo%2Fmerchandising&iid=M_d565fbf6-0263-4fce-b0b8-48e083237d0e_1_372UD5BXDFYS_MC.9JGNW7M0TUHD&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Electronics~Audio~All_9JGNW7M0TUHD&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=9JGNW7M0TUHD
+	url: https://www.flipkart.com/audio-video/~cs-53mrbtcuf5/pr?sid=0pm&collection-tab-name=Audio+And+Video&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&p%5B%5D=facets.availability%255B%255D%3DExclude%2BOut%2Bof%2BStock&p%5B%5D=facets.rating%255B%255D%3D4%25E2%2598%2585%2B%2526%2Babove&fm=neo%2Fmerchandising&iid=M_d565fbf6-0263-4fce-b0b8-48e083237d0e_1_372UD5BXDFYS_MC.9JGNW7M0TUHD&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Electronics~Audio~All_9JGNW7M0TUHD&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=9JGNW7M0TUHD
+
+Request Headers
+	Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+	Accept-Encoding: gzip, deflate, br
+	Accept-Language: en-GB,en;q=0.9
+	Cache-Control: max-age=0
+	Connection: keep-alive
+	Cookie: T=TI164690976709200136442811641182368091846287880698753774779009254689; gpv_pn=Store%20%3AAudio%20%26%20Video; gpv_pn_t=Store%20Browse; s_cc=true; AMCVS_17EB401053DAF4840A490D4C%40AdobeOrg=1; AMCV_17EB401053DAF4840A490D4C%40AdobeOrg=-227196251%7CMCIDTS%7C19062%7CMCMID%7C06678639771266144523071783981739236810%7CMCAID%7CNONE%7CMCOPTOUT-1646916971s%7CNONE; S=d1t11Xz9jd3U/Yz9UVz8/YD8/Pyj7PtT7BgUxVmqusl2swR86bRgAdp+V4/x+fs1zb+K4PTjVMhU1jAPQiDJ/ZJhw2w==; SN=VI895C6422BAA34FC785783C302CDB52D1.TOKD98DB734B5A7454297AFF74CEA1ABB49.1646909772.LO
+	Host: www.flipkart.com
+	Sec-Fetch-Dest: document
+	Sec-Fetch-Mode: navigate
+	Sec-Fetch-Site: none
+	Sec-Fetch-User: ?1
+	Sec-GPC: 1
+	Upgrade-Insecure-Requests: 1
+	User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36
+
+Response Headers
+	Content-Encoding: gzip
+	Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-eval' https://*.flixcart.com https://*.flixcart.net https://flipkart.d1.sc.omtrdc.net https://dpm.demdex.net https://tnc.phonepe.com https://captcha.px-cdn.net 'nonce-6508975343455534085'; style-src 'self' 'unsafe-inline' https://*.flixcart.com https://tnc.phonepe.com https://*.flixcart.net; img-src 'self' data: blob: https://*.flixcart.com https://*.flixcart.net https://images.ixigo.com https://flipkart.d1.sc.omtrdc.net https://www.facebook.com https://*.fkapi.net https://googleads.g.doubleclick.net https://www.google.com https://www.google.co.in https://www.googleadservices.com https://sp.analytics.yahoo.com https://bat.bing.com https://bat.r.msn.com https://pay.payzippy.com https://1.pay.payzippy.com https://tnc.phonepe.com; font-src 'self' data: https://*.flixcart.com https://tnc.phonepe.com https://*.flixcart.net; frame-src 'self' https://*.flipkart.com http://*.flipkart.com https://*.youtube.com https://youtube.com https://*.vimeo.com https://pay.payzippy.com https://1.pay.payzippy.com https://tnc.phonepe.com https://cdemux.appspot.com 'nonce-6508975343455534085'; worker-src 'self' https://*.flipkart.com blob:; child-src 'self' https://*.flipkart.com 'nonce-6508975343455534085'; connect-src 'self' *; object-src 'none'; base-uri 'self'; report-uri https://sentry.flipkart.com/api/69/security/?sentry_key=8c5053b8efc541dca3d22ce55f00a82e&sentry_release=4.81.1
+	Content-Type: text/html; charset=utf-8
+	Date: Thu, 10 Mar 2022 10:56:16 GMT
+	Server: nginx
+	Set-Cookie: T=TI164690976709200136442811641182368091846287880698753774779009254689; Max-Age=31536000; Domain=flipkart.com; Path=/; Expires=Fri, 10 Mar 2023 10:56:15 GMT; Secure; SameSite=None
+	Set-Cookie: T=TI164690976709200136442811641182368091846287880698753774779009254689; Max-Age=31536000; Domain=flipkart.net; Path=/; Expires=Fri, 10 Mar 2023 10:56:15 GMT; Secure; SameSite=None
+	Set-Cookie: SN=VI895C6422BAA34FC785783C302CDB52D1.TOKD98DB734B5A7454297AFF74CEA1ABB49.1646909775.LO; Max-Age=15778476; Domain=flipkart.com; Path=/; Expires=Fri, 09 Sep 2022 01:50:52 GMT; HttpOnly
+	Transfer-Encoding: chunked
+	X-Content-Type-Options: nosniff
+	x-e-id: 10.33.114.199
+	X-Frame-Options: SAMEORIGIN
+	X-XSS-Protection: 1; mode=block
+
+Response cookies
+	Set-Cookie: T=TI164690976709200136442811641182368091846287880698753774779009254689; Max-Age=31536000; Domain=flipkart.com; Path=/; Expires=Fri, 10 Mar 2023 10:56:15 GMT; Secure; SameSite=None
+	Set-Cookie: T=TI164690976709200136442811641182368091846287880698753774779009254689; Max-Age=31536000; Domain=flipkart.net; Path=/; Expires=Fri, 10 Mar 2023 10:56:15 GMT; Secure; SameSite=None
+	Set-Cookie: SN=VI895C6422BAA34FC785783C302CDB52D1.TOKD98DB734B5A7454297AFF74CEA1ABB49.1646909775.LO; Max-Age=15778476; Domain=flipkart.com; Path=/; Expires=Fri, 09 Sep 2022 01:50:52 GMT; HttpOnly
+
+Request method type
+	Request Method: GET
+
+Response status code
+	Status Code: 200 OK
